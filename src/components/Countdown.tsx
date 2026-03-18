2a5ed46a2feecf770ab4ba4fdf71be3abcf1a497
Gusti-fad <99389405+Gusti-fad@users.noreply.github.com>
2026-03-18 18:56:33 +0700

    change launch date

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -6,7 +6,7 @@ import duration from "dayjs/plugin/duration";
 
 dayjs.extend(duration);
 
-const target = dayjs("2026-03-18T18:00:00");
+const target = dayjs("2026-03-20T18:00:00");
 
 export default function Countdown() {
   const [time, setTime] = useState(getTime());
