b8763d7bc9945fe2916b368fd311efe60f13e408
Gusti-fad <gfadhillah06@gmail.com>
2026-03-26 04:58:20 +0700

    update countdown target date to March 27, 2026

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -6,7 +6,7 @@ import duration from "dayjs/plugin/duration";
 
 dayjs.extend(duration);
 
-const target = dayjs("2026-03-25T23:59:59");
+const target = dayjs("2026-03-27T23:59:59");
 
 export default function Countdown() {
   const [time, setTime] = useState(getTime());
